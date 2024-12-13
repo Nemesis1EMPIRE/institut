659c3c3642cec98af1e049935a9523a9a4c2a216
Nemesis1EMPIRE <137032212+Nemesis1EMPIRE@users.noreply.github.com>
2024-12-13 08:39:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript pour la recherche (facultatif pour l'instant)
-document.querySelector('.search-button').addEventListener('click', function() {
-    const searchQuery = document.getElementById('searchBar').value;
-    alert(`Recherche pour: ${searchQuery}`);
-});
-    
